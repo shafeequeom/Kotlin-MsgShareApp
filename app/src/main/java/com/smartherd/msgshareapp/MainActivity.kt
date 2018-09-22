9635185b1abaa767e5d1f78a80b2e2cf27d08d9b
shafeequeom <shafeequeom7@gmail.com>
2018-09-22 13:12:50 +0530

    Second Activity

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.smartherd.msgshareapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.smartherd.msgshareapp.R.id.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity","Button Was Clicked")
+            Toast.makeText(this,"Button Was Clicked",Toast.LENGTH_SHORT).show()
+        }
+
+        nextActivity.setOnClickListener{
+            val message = etUserMessage.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
