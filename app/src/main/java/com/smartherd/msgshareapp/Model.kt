f155abe24ef05e96cf1aec8a4e4522abaeb7afff
shafeequeom <shafeequeom7@gmail.com>
2018-09-22 18:12:04 +0530

    Hobbies List View

---
+++ app/src/main/java/com/smartherd/msgshareapp/Model.kt
@@ -0,0 +1,23 @@
+package com.smartherd.msgshareapp
+
+data class Hobby (var title: String)
+
+object Supplier{
+    val hobbies = listOf<Hobby>(
+            Hobby("swimming"),
+            Hobby("Reading"),
+            Hobby("Football"),
+            Hobby("Android"),
+            Hobby("coding"),
+            Hobby("swimming"),
+            Hobby("Reading"),
+            Hobby("Football"),
+            Hobby("Android"),
+            Hobby("coding"),
+            Hobby("swimming"),
+            Hobby("Reading"),
+            Hobby("Football"),
+            Hobby("Android"),
+            Hobby("coding")
+    )
+}
