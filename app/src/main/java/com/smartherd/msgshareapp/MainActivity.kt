61aed685014b37be95b04a9bfd86187d778ad899
shafeequeom <shafeequeom7@gmail.com>
2018-09-22 14:17:02 +0530

    Share to other apps button

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -28,5 +28,17 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("user_message", message)
             startActivity(intent)
         }
+        shareButton.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            val intent = Intent()
+
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT,message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Share To:ShafeeS"))
+
+
+        }
     }
 }
