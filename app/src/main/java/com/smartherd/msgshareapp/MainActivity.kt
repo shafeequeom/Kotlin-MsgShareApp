3c5742720fe230669d0e70a46b38cefdd42333d5
shafeequeom <shafeequeom7@gmail.com>
2018-09-22 13:49:46 +0530

    Passing data from one activity to another

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import com.smartherd.msgshareapp.R.id.*
+//import com.smartherd.msgshareapp.R.id.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,17 +15,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnShowToast.setOnClickListener {
-            Log.i("MainActivity","Button Was Clicked")
-            Toast.makeText(this,"Button Was Clicked",Toast.LENGTH_SHORT).show()
+            Log.i("MainActivity", "Button was clicked !")
+            Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
         }
 
         nextActivity.setOnClickListener{
-            val message = etUserMessage.text.toString()
+            val message: String = etUserMessage.text.toString()
             Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this,SecondActivity::class.java)
+
+            intent.putExtra("user_message", message)
             startActivity(intent)
         }
-
     }
 }
